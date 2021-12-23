acceab7c1284c750534c854b5d64aa3e240ca4ad
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-12-23 13:42:50 +0545

    demo: add knobs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,9 +7,14 @@
 
 	const search_in = Object.keys(tutorials);
 	let value = '';
+	let threshold = 0.3;
+	let limit = 5;
 
-	$: results = JSON.stringify(trgm_search(value, search_in, { limit: 1, threshold: 0.4 }), null, 4);
+	$: results = JSON.stringify(trgm_search(value, search_in, { limit, threshold }), null, 4);
 </script>
 
-<input type="text" bind:value />
+<input type="text" bind:value /> <br />
+Limit: <input type="number" bind:value={limit} /><br />
+Threshold: <input type="range" bind:value={threshold} min="0" max="1" step="0.1" />
+{threshold} <br />
 <pre><code>{results}</code></pre>
