ebdd4e9808ffb5e3047e9420169d5492f26e92f1
gtm-nayan <gtmnayan@gmail.com>
2022-04-19 10:27:52 +0545

    refactor forEach to a for loop

--- src/lib/trigrams.ts
+++ src/lib/trigrams.ts
@@ -7,16 +7,13 @@
  */
 export function find_distinct_trigrams(text: string): Set<string> {
 	const trigrams = new Set<string>();
-	text
-		.toLowerCase()
-		.split(/[^a-z]+/)
-		.forEach((word) => {
-			if (word.length) {
-				const padded = `  ${word} `;
-				for (let i = 3; i <= padded.length; ++i) {
-					trigrams.add(padded.slice(i - 3, i));
-				}
+	for (const word of text.toLowerCase().split(/[^a-z]+/)) {
+		if (word.length) {
+			const padded = `  ${word} `;
+			for (let i = 3; i <= padded.length; ++i) {
+				trigrams.add(padded.slice(i - 3, i));
 			}
-		});
+		}
+	}
 	return trigrams;
 }
